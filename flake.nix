55d1df0e4db279688876881049adcfc6063eee81
paaradiso <paaradiso@swag.horse>
2025-01-15 23:06:19 +1030

    install home-manager as a module in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,14 +2,23 @@
   description = "my nixos";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     zen-browser.url = "github:MarceColl/zen-browser-flake";
   };
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.utopia = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.alpha = import ./home.nix;
+        }
       ];
     };
   };
