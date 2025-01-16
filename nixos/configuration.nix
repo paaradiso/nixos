e8d3cf4f4bb2eda8f4e79db976b3e76c96a38372
paaradiso <paaradiso@swag.horse>
2025-01-16 20:04:47 +1030

    fix spacing

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -31,7 +31,6 @@
     };
   };
 
-
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -185,6 +184,5 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.11"; # Did you read the comment?
-
 }
 
