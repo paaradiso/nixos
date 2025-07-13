171b08584279649da1a1c4c9ee05d9b53955224f
paaradiso <38374917+paaradiso@users.noreply.github.com>
2025-07-13 20:32:37 +0930

    automatically add git ssh key to agent on login

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -1,4 +1,11 @@
 {
   services.openssh.enable = true;
   programs.ssh.startAgent = true;
+
+  systemd.user.services.add_git_key = {
+    script = ''
+      ssh-add $HOME/.ssh/git_key
+    '';
+    wantedBy = ["multi-user.target"];
+  };
 }
