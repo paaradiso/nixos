c18c58656685003c029958639753558c61e01cf3
paaradiso <38374917+paaradiso@users.noreply.github.com>
2025-07-13 20:48:23 +0930

    fix git ssh key service (it still doesnt work)

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -1,11 +1,15 @@
-{
+{user, ...}: {
   services.openssh.enable = true;
   programs.ssh.startAgent = true;
 
   systemd.user.services.add_git_key = {
-    script = ''
-      ssh-add $HOME/.ssh/git_key
-    '';
-    wantedBy = ["multi-user.target"];
+    description = "Add Git SSH key to agent";
+    wantedBy = ["default.target"];
+    after = ["ssh-agent.service"];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "/run/current-system/sw/bin/ssh-add /home/${user}/.ssh/git_key";
+      Environment = "SSH_AUTH_SOCK=%t/ssh-agent";
+    };
   };
 }
