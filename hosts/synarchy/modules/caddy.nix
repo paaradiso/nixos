1f1b50b3f7298285e3901db9389a6ab596360e48
paaradiso <38374917+paaradiso@users.noreply.github.com>
2025-09-27 14:21:38 +0930

    update caddy

--- hosts/synarchy/modules/caddy.nix
+++ hosts/synarchy/modules/caddy.nix
@@ -28,7 +28,7 @@ with lib; {
         plugins = [
           "github.com/caddy-dns/cloudflare@v0.2.2-0.20250506153119-35fb8474f57d"
         ];
-        hash = "sha256-60yLaAEyjdcGokoi4/n90u2z/ic3kz7dCREhx9u/gGM=";
+        hash = "sha256-E2yLC+xw/DHswFkyrx5dK700L1iPp+XDlTnWug/OTcg=";
       };
 
       environmentFile = config.age.secrets.caddy_env.path;
