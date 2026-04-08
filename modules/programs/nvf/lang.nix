e0f9a9124cce2adcfb22f7d74063a16129f1dbb5
paaradiso <38374917+paaradiso@users.noreply.github.com>
2026-04-09 00:15:45 +0930

    nvf: add rustywind to sort tailwind classes in gleam

--- modules/programs/nvf/lang.nix
+++ modules/programs/nvf/lang.nix
@@ -1,43 +1,47 @@
 {pkgs, ...}: {
+  environment.systemPackages = with pkgs; [rustywind];
   programs.nvf.settings.vim = {
     treesitter = {
       grammars = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
         typescript
       ];
     };
-
     autocomplete = {
       nvim-cmp = {
         enable = true;
       };
     };
-
     lsp = {
       enable = true;
     };
-
     languages = {
       enableTreesitter = true;
-
       nix.enable = true;
       java.enable = true;
-
       ts.enable = true;
       svelte.enable = true;
       css.enable = true;
-
       clang.enable = true;
-
       gleam.enable = true;
     };
-
     formatter = {
       conform-nvim = {
         enable = true;
         setupOpts = {
           format_on_save = {
-            lsp_format = "fallback";
+            lsp_format = "first";
             timeout_ms = 500;
+          };
+          formatters = {
+            rustywind_gleam = {
+              command = "${pkgs.rustywind}/bin/rustywind";
+              args = [
+                "--custom-regex"
+                "attribute\\.class\\([\\s\\S]*?[\\\"\\']([\\s\\S]*?)[\\\"\\'][\\s\\S]*?\\)"
+                "--stdin"
+              ];
+              stdin = true;
+            };
           };
           formatters_by_ft = {
             nix = ["alejandra"];
@@ -47,18 +51,17 @@
             c = ["clang-format"];
             cpp = ["clang-format"];
             sql = ["pg_format"];
+            gleam = ["rustywind_gleam"];
           };
         };
       };
     };
-
     debugger = {
       nvim-dap = {
         enable = true;
         ui.enable = true;
       };
     };
-
     augroups = [
       {
         name = "setIndent";
